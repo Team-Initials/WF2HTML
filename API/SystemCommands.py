29ff8df124d7e128348272d4d0e8d119824d20d0
rdatta95 <rdatta95@protonmail.com>
2018-04-25 23:30:56 +0530

    Added commands for Linux(Ubuntu)

---
+++ API/SystemCommands.py
@@ -0,0 +1,18 @@
+"""
+This file contains the command-line/terminal commands used to launch different applications
+Supports both linux( currently only Ubuntu ) and windows command.
+"""
+
+SystemCommands = {
+
+    "defaultImgEditor": {
+        "nt": "mspaint",
+        "posix": "gimp"
+    },
+
+    "defaultBrowser": {
+        "nt": "start",
+        "posix": "xdg-open"
+    }
+
+}
